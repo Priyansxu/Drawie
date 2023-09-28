afa5c95d0c7059e5738db4596d86e3716a9e7956
Gyandeep Aryan <gyandeeparyan2@gmail.com>
2023-09-28 16:06:02 +0530

    import updated

--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
-import menuSlice from "./slices/menuSlice";
-import toolboxSlice from "./slices/toolboxSlice";
+import menuSlice from "@/slices/menuSlice";
+import toolboxSlice from "@/slices/toolBoxSlice";
 export const store = configureStore({
   reducer: {
     menu: menuSlice,
