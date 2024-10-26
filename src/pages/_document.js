9dd2745caf023632498364a192bffca3dff3ceae
Gyandeep Aryan <gyandeeparyan.dev@gmail.com>
2024-10-26 19:37:26 +0530

    pwa meta data updated

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="title" content="Aakriti" />
+        <meta name="description" content="Aakriti" />
+        <meta name="generator" content="Next.js" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="keywords" content="nextjs, next14, pwa, next-pwa" />
+      </Head>
       <body>
         <Main />
         <NextScript />
