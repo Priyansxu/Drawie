1088e026930e81e2d18968e4f8efa1cf7962e929
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2024-12-17 19:15:54 +0530

    Update index.js

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,13 +1,11 @@
 import { ArrowDownCircle, Eraser, Pencil, Redo, Undo } from "lucide-react";
-import styles from "./index.module.css";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { menuItemClick, actionItemClick } from "@/slices/menuSlice";
-import { useSelector } from "react-redux";
 import { MENU_ITEMS } from "@/constants";
 import cx from "classnames";
+
 const Menu = () => {
   const activeMenuItem = useSelector((store) => store.menu.activeMenuItem);
-
   const dispatch = useDispatch();
 
   const handleClick = (menuItem) => {
@@ -19,47 +17,48 @@ const Menu = () => {
   };
 
   return (
-    <div>
- <div className={styles.menuContainer}>
+    <div className="absolute px-5 py-1 flex justify-between md:w-1/4 left-1/2 top-10 rounded-xl md:rounded-md border border-border1 bg-background1 shadow-shadow1 transform -translate-x-1/2">
       <div
-        className={cx(styles.iconWrapper, {
-          [styles.active]: activeMenuItem === MENU_ITEMS.PENCIL,
-        })}
-        onClick={() => {
-          handleClick(MENU_ITEMS.PENCIL);
-        }}>
-        <Pencil className={styles.icon} />
+        className={cx(
+          "cursor-pointer flex justify-center items-center h-10 w-10 rounded-md",
+          { "bg-text2": activeMenuItem === MENU_ITEMS.PENCIL }
+        )}
+        onClick={() => handleClick(MENU_ITEMS.PENCIL)}
+      >
+        <Pencil className="text-text1 text-[20px]" />
       </div>
+
       <div
-        className={cx(styles.iconWrapper, {
-          [styles.active]: activeMenuItem === MENU_ITEMS.ERASER,
-        })}
-        onClick={() => {
-          handleClick(MENU_ITEMS.ERASER);
-        }}>
-        <Eraser className={styles.icon} />
+        className={cx(
+          "cursor-pointer flex justify-center items-center h-10 w-10 rounded-md",
+          { "bg-text2": activeMenuItem === MENU_ITEMS.ERASER }
+        )}
+        onClick={() => handleClick(MENU_ITEMS.ERASER)}
+      >
+        <Eraser className="text-text1 text-[20px]" />
       </div>
+
       <div
-        className={styles.iconWrapper}
-        onClick={() => handleActionItemClick(MENU_ITEMS.UNDO)}>
-        <Undo className={styles.icon} />
+        className="cursor-pointer flex justify-center items-center h-10 w-10 rounded-md"
+        onClick={() => handleActionItemClick(MENU_ITEMS.UNDO)}
+      >
+        <Undo className="text-text1 text-[20px]" />
       </div>
+
       <div
-        className={styles.iconWrapper}
-        onClick={() => handleActionItemClick(MENU_ITEMS.REDO)}>
-        <Redo className={styles.icon} />
+        className="cursor-pointer flex justify-center items-center h-10 w-10 rounded-md"
+        onClick={() => handleActionItemClick(MENU_ITEMS.REDO)}
+      >
+        <Redo className="text-text1 text-[20px]" />
       </div>
+
       <div
-        className={styles.iconWrapper}
-        onClick={() => handleActionItemClick(MENU_ITEMS.DOWNLOAD)}>
-        <ArrowDownCircle className={styles.icon} />
+        className="cursor-pointer flex justify-center items-center h-10 w-10 rounded-md"
+        onClick={() => handleActionItemClick(MENU_ITEMS.DOWNLOAD)}
+      >
+        <ArrowDownCircle className="text-text1 text-[20px]" />
       </div>
-     
     </div>
-   
-    </div>
-   
-    
   );
 };
 
