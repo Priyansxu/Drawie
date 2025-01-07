293f01c72be69b684aee129c1154efa33a33b8ee
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2025-01-07 21:56:35 +0530

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -5,8 +5,16 @@ const COLORS = {
     BLUE: 'rgb(128, 128, 255)',      // Light blue (a lighter shade of blue)
     ORANGE: 'rgb(255, 192, 128)',    // Light orange (a lighter shade of orange)
     YELLOW: 'rgb(255, 255, 128)',    // Light yellow (a lighter shade of yellow)
-    WHITE: 'white'
-}
+    PURPLE: 'rgb(192, 128, 255)',    // Light purple (a lighter shade of purple)
+    PINK: 'rgb(255, 182, 193)',      // Light pink (a lighter shade of pink)
+    CYAN: 'rgb(128, 255, 255)',      // Light cyan (a lighter shade of cyan)
+    BROWN: 'rgb(210, 180, 140)',     // Light brown (tan)
+    GRAY: 'rgb(169, 169, 169)',      // Light gray (standard gray)
+    TEAL: 'rgb(128, 192, 192)',      // Light teal
+    GOLD: 'rgb(255, 215, 128)',      // Light gold
+    LIME: 'rgb(192, 255, 128)',      // Light lime
+    WHITE: 'white'                   // Pure white
+};
 
 const MENU_ITEMS = {
     PENCIL: 'PENCIL',
