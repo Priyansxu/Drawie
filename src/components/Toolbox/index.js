f64a05fccba91254bba6970d915e9c17e188a8a9
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2025-01-07 22:40:51 +0530

    Update index.js

--- src/components/Toolbox/index.js
+++ src/components/Toolbox/index.js
@@ -1,5 +1,3 @@
-'use client'
-
 import React, { useState, useRef, useEffect } from 'react';
 import { useSelector, useDispatch } from "react-redux";
 import { COLORS, MENU_ITEMS } from "@/constants";
