41257c62400579109bfbebbd00942ab6966610d2
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2025-01-08 21:17:30 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Board from "@/components/Board";
 import Menu from "@/components/Menu";
 import Toolbox from "@/components/Toolbox";
@@ -6,6 +7,12 @@ import Toolbox from "@/components/Toolbox";
 export default function Home() {
   return (
     <>
+   <Head>
+    <title>Drawie</title>
+    <meta name="description" content="Create art together online!" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="icon" href="/logo.jpg" />
+   </Head>
     <Menu/>
    <Toolbox/>
    <Board/>
