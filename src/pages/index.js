df3299b99227860cda43cae96a22014b6cbf6be0
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2025-01-08 21:20:09 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,13 +5,13 @@ import Toolbox from "@/components/Toolbox";
 
 
 export default function Home() {
-  return (
+  return 
     <>
-   <Head>
-    <title>Drawie</title>
-    <meta name="A simple app that allows you to create and customize digital drawings with ease." />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="icon" href="/logo.jpg" />
+  <Head>
+   <title>Drawie</title>
+   <meta name="A simple app that allows you to create and customize digital drawings with ease." />
+   <meta name="viewport" content="width=device-width, initial-scale=1" />
+   <link rel="icon" href="/logo.jpg" />
    </Head>
     <Menu/>
    <Toolbox/>
